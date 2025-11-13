5f4c2901655b628607579a5a9917a33d2e7dbba9
Porfirio Salazar López <157189199+Porfirio-Salazar-Lopez@users.noreply.github.com>
2025-11-12 21:32:44 -0600

    Agregando clases Persona y cancion

---
+++ src/main/java/modelos/Usuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author porfi
+ */
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String password;
+
+    public Usuario(int id, String nombre, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
